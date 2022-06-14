66b78e780c6b09c310c41e9682d1d5c1b2af70d1
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2022-06-14 22:15:03 +0700

    make it better

--- client/src/app/templates/more-page/login-register/login/login.component.ts
+++ client/src/app/templates/more-page/login-register/login/login.component.ts
@@ -1,3 +1,4 @@
+import { urlVerify } from './../../../../../environments/environment';
 import { UserProfile } from '../../../../payload/UserProfile';
 import { AuthenticationResponse } from 'src/app/payload/AuthenticationResponse';
 import { AppCookieService } from '../../../../service/appCookie/app-cookie.service';
@@ -7,6 +8,7 @@ import { HttpServiceService } from '../../../../service/httpService/http-service
 
 import { Component, OnInit } from '@angular/core';
 import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
+import { Location } from '@angular/common';
 
 
 @Component({
@@ -17,67 +19,67 @@ import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthService } from 'a
 export class LoginComponent implements OnInit {
   myScriptElement!: HTMLScriptElement;
   loginForm!: FormGroup;
-  error!:string;
-  constructor(private authService: SocialAuthService, private http: HttpServiceService, private router:Router, private appCookie:AppCookieService) { 
+  error!: string;
+  constructor(private authService: SocialAuthService, private http: HttpServiceService, private router: Router, private appCookie: AppCookieService, public _location: Location) {
   }
 
   ngOnInit(): void {
     this.http.setTitle("E Store-Login")
     this.myScriptElement = document.createElement("script");
-    this.myScriptElement.src =  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/js/all.min.js";
+    this.myScriptElement.src = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/js/all.min.js";
     document.body.appendChild(this.myScriptElement);
     this.loginForm = new FormGroup({
-      email : new FormControl(),
+      email: new FormControl(),
       password: new FormControl()
     })
   }
 
-  loginUserCheck(){
-    let request={
-        "email":this.loginForm.get("email")?.value ,
-        "password": this.loginForm.get("password")?.value
+  loginUserCheck() {
+    let request = {
+      "email": this.loginForm.get("email")?.value,
+      "password": this.loginForm.get("password")?.value
     }
-    if(request.email == null  || request.email == ""|| request.password == null ||  request.password == "")
+    if (request.email == null || request.email == "" || request.password == null || request.password == "")
       return;
-    this.http.loginRequest("/login/user", request).subscribe(data=>{
+    this.http.loginRequest("/login/user", request).subscribe(data => {
       this.setLogin(data);
-    }, error=>{
-      this.error=error.error.message;
+    }, error => {
+      this.error = error.error.message;
     })
   }
   signInWithGoogle(): void {
-    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(data=>{
-      this.http.loginRequest("/login/user/google", {value:data.idToken}).subscribe(data=>{
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then(data => {
+      this.http.loginRequest("/login/user/google", { value: data.idToken }).subscribe(data => {
         this.setLogin(data);
-      },error=>{
-        this.error=error.error.message;
+      }, error => {
+        this.error = error.error.message;
       });
     });
   }
 
   signInWithFB(): void {
-    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(data=>{
-      this.http.loginRequest("/login/user/facebook", {value:data.authToken}).subscribe(data=>{
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(data => {
+      this.http.loginRequest("/login/user/facebook", { value: data.authToken }).subscribe(data => {
         this.setLogin(data);
-      },error=>{
-        this.error=error.error.message;
+      }, error => {
+        this.error = error.error.message;
       });
     });
   }
 
-  setLogin(data:AuthenticationResponse){
+  setLogin(data: AuthenticationResponse) {
     this.appCookie.set("authenticationToken", data.authenticationToken)
-    this.http.setLoginData (data.userProfile);
+    this.http.setLoginData(data.userProfile);
     localStorage.setItem("search key", "");
     this.http.sendClickSubject();
     this.http.sendSubjectBottomBarForLogin();
-    this.router.navigateByUrl("/home");
+    location.replace("home");
   }
 
-  setUpdate(userProfile:UserProfile){
-    this.http.setLoginData (userProfile);
+  setUpdate(userProfile: UserProfile) {
+    this.http.setLoginData(userProfile);
     localStorage.setItem("search key", "");
-    alert("Successfully" );
+    alert("Successfully");
     this.http.sendClickSubject();
   }
 }
--- client/src/app/templates/products/products.component.ts
+++ client/src/app/templates/products/products.component.ts
@@ -5,14 +5,14 @@ import { ProductPayload } from './../../payload/ProductPayload';
 import { Subscription } from 'rxjs';
 import { HttpServiceService } from './../../service/httpService/http-service.service';
 import { Component, OnInit } from '@angular/core';
-import { Location} from '@angular/common';
+import { Location } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 
-interface sortItem{
-  id:number;
-  name:string;
-  value:string;
-  oldOrNew:boolean;
+interface sortItem {
+  id: number;
+  name: string;
+  value: string;
+  oldOrNew: boolean;
 }
 
 @Component({
@@ -22,150 +22,187 @@ interface sortItem{
 })
 
 export class ProductsComponent implements OnInit {
-  searAndSort!:SearchAndSortPayload;
+  searAndSort!: SearchAndSortPayload;
   products!: ProductPayload[];
-  lastPage!:number;
+  lastPage!: number;
   totalElements = 0;
   active = false;
 
-  cateName="";
-  
+  priceRange: any[] = []
 
+  cateName = "";
   //sort
-  sortKey="Newest";
+  sortKey = "Newest";
+  sortKeyPrice = "";
   sortId = 1;
-  sortKeys:sortItem[] =[
-    {id: 1, name:"Newest", value:"id", oldOrNew: true},
-    {id:2, name:"Oldest", value:"id",oldOrNew: false},
-    {id: 3, name:"Popular", value:"totalReview5Star",oldOrNew: true},
-    {id: 4, name:"Most sale", value:"unitSold",oldOrNew: true}
+  sortPriceRangeId = 0;
+
+  sortKeys: sortItem[] = [
+    { id: 1, name: "Newest", value: "id", oldOrNew: true },
+    { id: 2, name: "Oldest", value: "id", oldOrNew: false },
+    { id: 3, name: "Popular", value: "totalReview5Star", oldOrNew: true },
+    { id: 4, name: "Most sale", value: "unitSold", oldOrNew: true }
   ]
-  checkSort=false;
+  checkSort = false;
   //sort
   //page
-  previousNext !:number;
-  selectedItem !:number;
+  previousNext !: number;
+  selectedItem !: number;
   //end page
 
-  changeProductByCategory! : Subscription;
+  changeProductByCategory!: Subscription;
   baseUrl = environment.urlServe;
   j = 0;
-  
-  constructor(private router:Router,private activatedRouter:ActivatedRoute,private _location: Location,public http: HttpServiceService, private productService:ProductService){}
+  priceRangeFn() {
+    let end = 0;
+    for (let index = 0; index < 10; index++) {
+      const element = { id: index + 1, start: end + 1, end: end += 50 };
+      this.priceRange.push(element);
+    }
+    const element = { id: this.priceRange.length + 1, start: end, end: null };
+    this.priceRange.push(element);
+  }
+
+  constructor(private router: Router, private activatedRouter: ActivatedRoute, private _location: Location, public http: HttpServiceService, private productService: ProductService) {
+    this.priceRangeFn();
+  }
   ngOnInit(): void {
     this.previousNext = this.selectedItem = 1
-    this.activatedRouter.queryParams.subscribe(data=>{
+    this.activatedRouter.queryParams.subscribe(data => {
       this.searAndSort = new SearchAndSortPayload();
-      if(data.key != null){
+      if (data.key != null) {
         this.searAndSort.key = data.key;
       }
-      if(data.value != null)
+      if (data.value != null) {
         this.searAndSort.sortKey = data.value;
-      else{
-        this.searAndSort.sortKey = "id";
-        this.sortKey="Newest";
-        this.sortId = 1;
+        if (data.sortId != null) {
+          this.sortKey = this.sortKeys[data.sortId - 1].name
+          this.sortId = this.sortKeys[data.sortId - 1].id;
+        }
+        if (data.sortPriceId != null) {
+          let sortPrice = this.priceRange[data.sortPriceId - 1];
+          this.sortPriceRangeId = sortPrice.id;
+          sortPrice.end == null ? this.sortKeyPrice = "more than " + sortPrice.start : this.sortKeyPrice = sortPrice.start + "-" + sortPrice.end;
+
+        }
       }
-        
-      if(data.icate != null){
+      else {
+        this.searAndSort.sortKey = "id";
+        this.sortKey = "Newest";
+        this.sortKeyPrice = "";
+        this.sortId = 1;
+        this.sortPriceRangeId = 0;
+      }
+
+      if (data.icate != null) {
         this.searAndSort.cateId = data.icate;
       }
-      if(data.page != null && data.page > 0){
-        this.previousNext = this.selectedItem =data.page;
+      if (data.page != null && data.page > 0) {
+        this.previousNext = this.selectedItem = data.page;
       }
-      if(data.newest != null){
-        this.searAndSort.oldOrNew =data.newest;
+      if (data.newest != null) {
+        this.searAndSort.oldOrNew = data.newest;
       }
-      this.searAndSort.page = this.previousNext -1
+      this.searAndSort.page = this.previousNext - 1
       this.getPage();
-      if(data.caterory != null){
-        this.cateName = data.caterory; 
+      if (data.caterory != null) {
+        this.cateName = data.caterory;
         this.active = true;
       }
       else
-      this.active = false;
+        this.active = false;
     })
     this.http.setTitle("E Store-Products")
 
   }
-  clickCartPlus(productPayload:ProductPayload){
-    if(!this.http.checkLogin())
-      return 
-   this.productService.addCart(productPayload, 1)
+  clickCartPlus(productPayload: ProductPayload) {
+    if (!this.http.checkLogin())
+      return
+    this.productService.addCart(productPayload, 1)
   }
 
-  clickProductDetail(productId:string){
+  clickProductDetail(productId: string) {
     this.http.clickDetailProduct(productId);
   }
-  clickWishListPlus(item:ProductPayload){
-    if(!this.http.checkLogin())
-      return 
-   this.productService.addWishList(item)
+  clickWishListPlus(item: ProductPayload) {
+    if (!this.http.checkLogin())
+      return
+    this.productService.addWishList(item)
   }
 
-  getPage(){
-    this.http.postRequest("/product/search",this.searAndSort).subscribe(data=>{
+  getPage() {
+    this.http.postRequest("/product/search", this.searAndSort).subscribe(data => {
       this.totalElements = data.totalElements;
-      this.products=data.content;
+      this.products = data.content;
       this.lastPage = data.totalPages;
-      },error=>{
-        alert("Server connection error")
+    }, error => {
+      alert("Server connection error")
     })
-    window.scroll(0,0);
+    window.scroll(0, 0);
   }
 
-  clickPage(page:number){
+  clickPage(page: number) {
     this.previousNext = page;
-    this.page() 
+    this.page()
   }
-  page(){
+  page() {
     this.selectedItem = this.previousNext;
-    this.searAndSort.page = this.previousNext -1
-    this.router.navigate(['/products'], { queryParamsHandling:"merge",queryParams:{'page':this.previousNext}});
+    this.searAndSort.page = this.previousNext - 1
+    this.router.navigate(['/products'], { queryParamsHandling: "merge", queryParams: { 'page': this.previousNext } });
     this.getPage();
   }
-  nextPage(){
+  nextPage() {
     this.previousNext++;
-    if (this.previousNext > this.lastPage){
+    if (this.previousNext > this.lastPage) {
       this.previousNext = this.lastPage;
       return;
     }
-    
+
     this.page()
   }
 
-  previousPage(){
+  previousPage() {
     this.previousNext--
-    if (this.previousNext < 1){
+    if (this.previousNext < 1) {
       this.previousNext = 1;
       return;
     }
-   this.page() 
+    this.page()
   }
 
 
   backClicked() {
     this._location.back();
   }
-  
-  sort(item:sortItem){
+
+  sort(item: sortItem) {
     this.sortId = item.id;
     this.sortKey = item.name;
     this.reset()
-    this.router.navigate(['/products'], { queryParamsHandling:"merge",queryParams:{'page':this.previousNext, 'value': item.value, 'newest':item.oldOrNew}});
+    this.router.navigate(['/products'], { queryParamsHandling: "merge", queryParams: { 'page': this.previousNext, 'value': item.value, 'newest': item.oldOrNew, sortId: this.sortId } });
   }
 
-  search(){
+  sortPrice(item: any) {
+    this.sortPriceRangeId = item.id;
+    item.end == null ? this.sortKeyPrice = "more than " + item.start : this.sortKeyPrice = item.start + "-" + item.end;
     this.reset()
-    if(this.searAndSort.key.length < 3){
+    this.router.navigate(['/products'], { queryParamsHandling: "merge", queryParams: { 'page': this.previousNext, 'priceStart': item.start, 'priceEnd': item.end == null ? "more than" : item.end, sortPriceId: this.sortPriceRangeId } });
+  }
+
+  search() {
+    this.reset()
+    if (this.searAndSort.key.length < 3) {
       alert("Keyword too short.")
       return;
     }
-      
-    this.router.navigate(['/products'], { queryParamsHandling:"merge",queryParams:{'page':this.previousNext, 'key': this.searAndSort.key}});
+
+    this.router.navigate(['/products'], { queryParamsHandling: "merge", queryParams: { 'page': this.previousNext, 'key': this.searAndSort.key } });
   }
 
-  reset(){
-    this.previousNext =  this.selectedItem = 1;
+  reset() {
+    this.previousNext = this.selectedItem = 1;
+  }
+  sortPriceNone() {
+    this.router.navigate(['/products'], {queryParams: { 'page': this.previousNext } });
   }
 }
